6c307173daa24d6aa08e20f3698db8b0ac4a8069
tobeseen37-bit <tobeseen37@gmail.com>
2025-09-05 18:15:15 -0500

    Day 0 test commit

---
+++ test.js
@@ -0,0 +1,6 @@
+// Testing Codeium in JavaScript
+console.log("Hello, Codeium!");
+const numbers = [1, 2, 3, 4, 5];
+const squared = numbers.map(n => n * n);
+const sum =
+    squared.reduce((acc, val) => acc + val, 0);
